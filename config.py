1f163c21173fcabe1a0200311ce7f6c38f544bc5
Aashish Carmel Jai <aashishcarmeljai@gmail.com>
2025-06-23 21:58:58 +0530

    Railway Deploy Test

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass@localhost/cmms_db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
